157dc8c10adf3086ede58de7020ebe132880fa74
Huzaifa Aftab <75427330+huzaifa95-star@users.noreply.github.com>
2022-11-27 17:43:12 +0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -24,10 +24,11 @@ export default function Home({ posts }) {
 }
 
 // Fetch data at build time
-export async function getStaticProps() {
+export async function getServerSideProps() {
   const posts = (await getPosts()) || [];
+
   return {
-    props: { posts },
+    props: { posts }
   };
 }
 
