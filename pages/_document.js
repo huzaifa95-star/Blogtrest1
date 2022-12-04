c89af4e657e8b73e84ab5cb7fa91b0af1bcaaef1
Huzaifa Aftab <75427330+huzaifa95-star@users.noreply.github.com>
2022-12-04 14:29:53 +0500

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -10,9 +10,12 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9736994394968523"
-        crossorigin="anonymous"></script>
-        {/* <meta name="viewport" content="width=device-width,minimum-scale=1, initial-scale=1" /> */}
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9736994394968523"
+            crossOrigin="anonymous"
+          />
+          {/* <meta name="viewport" content="width=device-width,minimum-scale=1, initial-scale=1" /> */}
         </Head>
         <body>
           <Main />
